b03972044d35463df8555d7ed2310c577cff1816
lemonpaul <leamonpaul@yandex.ru>
2020-02-10 14:22:24 +0300

    Mark current track with '*'

--- playlist/static/playlist/player.js
+++ playlist/static/playlist/player.js
@@ -11,12 +11,14 @@ $(function() {
         paused = $('#audio')[0].paused;
         if (currentTrack !== 0)
         {
+            $('div#track_index')[currentTrack-1].innerText = currentTrack;
             if (currentTrack == $('#_list').find('div#track_title').length)
             {
                 currentTrack = 1;
             } else {
                 currentTrack++;
             }
+            $('div#track_index')[currentTrack-1].innerText = "*";
             $('#audio')[0].src = $('#track'+currentTrack).find('a#track_url')[0].href;
             $('#title')[0].innerText = $('#track'+currentTrack).find('div#track_artist')[0].innerText + " - " + $('#track'+currentTrack).find('div#track_title')[0].innerText;
             if (!paused) $('#audio')[0].play();
@@ -27,24 +29,28 @@ $(function() {
         paused = $('#audio')[0].paused;
         if (currentTrack !== 0)
         {
+            $('div#track_index')[currentTrack-1].innerText = currentTrack;
             if (currentTrack == 1)
             {
                 currentTrack = $('#_list').find('div#track_title').length;
             } else {
                 currentTrack--;
             }
+            $('div#track_index')[currentTrack-1].innerText = "*";
             $('#audio')[0].src = $('#track'+currentTrack).find('a#track_url')[0].href;
             $('#title')[0].innerText = $('#track'+currentTrack).find('div#track_artist')[0].innerText + " - " + $('#track'+currentTrack).find('div#track_title')[0].innerText;
             if (!paused) $('#audio')[0].play();
         }
     });
     $('#audio')[0].addEventListener("ended", function (e) {
+        $('div#track_index')[currentTrack-1].innerText = currentTrack;
         if (currentTrack == $('#_list').find('div#track_title').length)
         {
             currentTrack = 1;
         } else {
             currentTrack++;
         }
+        $('div#track_index')[currentTrack-1].innerText = "*";
         $('#audio')[0].src = $('#track'+currentTrack).find('a#track_url')[0].href;
         $('#title')[0].innerText = $('#track'+currentTrack).find('div#track_artist')[0].innerText + " - " + $('#track'+currentTrack).find('div#track_title')[0].innerText;
         $('#audio')[0].play();
