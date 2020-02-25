bcdc092cdf9f196f2556c7669727433a50deabbb
lemonpaul <leamonpaul@yandex.ru>
2020-02-26 00:12:01 +0300

    Remove media folder from urls

--- playlist/urls.py
+++ playlist/urls.py
@@ -30,4 +30,4 @@ urlpatterns = [
     path('vote/', views.vote, name='vote'),
     path('autocomplete/', views.autocomplete, name='autocomplete'),
     path('admin/', admin.site.urls),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
